389dc3c0b345c16b5abe6e6ff33d20682a03d2b4
iffn <iffn1990@gmail.com>
2023-03-31 12:04:08 +0200

    Added desktop controls for scaler

--- Scripts/MeshEditing/Controllers/Scaler.cs
+++ Scripts/MeshEditing/Controllers/Scaler.cs
@@ -15,6 +15,7 @@ namespace iffnsStuff.iffnsVRCStuff.MeshBuilder
         float referenceDistance;
 
         bool isScaling = false;
+        bool isInVR;
 
         Vector3 originalLocalPosition;
         Vector3 originalLocalScale;
@@ -31,6 +32,7 @@ namespace iffnsStuff.iffnsVRCStuff.MeshBuilder
             transform.localScale = Vector3.one;
 
             scaleObject.transform.localPosition = originalLocalPosition;
+            scaleObject.transform.localRotation = Quaternion.identity;
             scaleObject.transform.localScale = originalLocalScale;
         }
 
@@ -38,27 +40,55 @@ namespace iffnsStuff.iffnsVRCStuff.MeshBuilder
         {
             originalLocalPosition = scaleObject.transform.localPosition;
             originalLocalScale = scaleObject.transform.localScale;
+
+            isInVR = Networking.LocalPlayer.IsUserInVR();
         }
 
         private void Update()
         {
-            if (Input.GetAxis("Oculus_CrossPlatform_PrimaryHandTrigger") > 0.9f && Input.GetAxis("Oculus_CrossPlatform_SecondaryHandTrigger") > 0.9f)
-            {
-                if (!isScaling)
-                {
-                    SetupScaling();
-                    isScaling = true;
-                }
+            if (!Networking.IsOwner(gameObject)) return;
 
-                Scale();
+            if (isInVR)
+            {
+                if (Input.GetAxis("Oculus_CrossPlatform_PrimaryHandTrigger") > 0.9f && Input.GetAxis("Oculus_CrossPlatform_SecondaryHandTrigger") > 0.9f)
+                {
+                    if (!isScaling)
+                    {
+                        SetupScaling();
+                        isScaling = true;
+                    }
+
+                    Scale();
+                }
+                else
+                {
+                    if (isScaling)
+                    {
+                        StopScaling();
+                        isScaling = false;
+                    }
+                }
             }
             else
             {
-                if (isScaling)
-                {
-                    StopScaling();
-                    isScaling = false;
-                }
+                float movementSpeed = 0.3f * Time.deltaTime;
+                float rotationSpeed = 40f * Time.deltaTime;
+                float scalingSpeed = 1f + 0.3f * Time.deltaTime;
+
+                if (Input.GetKey(KeyCode.Keypad8)) transform.Translate(movementSpeed * Vector3.forward);
+                if (Input.GetKey(KeyCode.Keypad2)) transform.Translate(movementSpeed * Vector3.back);
+                if (Input.GetKey(KeyCode.Keypad4)) transform.Translate(movementSpeed * Vector3.left);
+                if (Input.GetKey(KeyCode.Keypad6)) transform.Translate(movementSpeed * Vector3.right);
+                if (Input.GetKey(KeyCode.PageUp)) transform.Translate(movementSpeed * Vector3.up);
+                if (Input.GetKey(KeyCode.PageDown)) transform.Translate(movementSpeed * Vector3.down);
+                
+                if (Input.GetKey(KeyCode.Keypad9)) transform.Rotate(rotationSpeed * Vector3.up);
+                if (Input.GetKey(KeyCode.Keypad7)) transform.Rotate(rotationSpeed * Vector3.down);
+                
+                if (Input.GetKey(KeyCode.KeypadPlus)) transform.localScale *= scalingSpeed;
+                if (Input.GetKey(KeyCode.KeypadMinus)) transform.localScale *= 1f/scalingSpeed;
+
+                if(Input.GetKeyDown(KeyCode.KeypadEnter)) ResetScale();
             }
         }
 
